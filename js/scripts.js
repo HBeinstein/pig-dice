cb59027611d1e338999de7d4e0c48406d2cedad4
Jeff Kim <kim2jy@hotmail.com>
2020-06-17 13:34:05 -0700

    Add user interface and JS script to allow players to roll dice, add score, and hold to add the round score to their total score
    Co-authored-by: Hannah Beinstein <hannah.beinstein@gmail.com>
    Co-authored-by: Ruthie Newman <ruthienewman@protonmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,7 @@
 // Business Logic
-function Player (playerName, currentScore, roundScore, totalScore){
+function Player (playerName, diceRoll, roundScore, totalScore){
   this.playerName = playerName,
-  this.currentScore = currentScore,
+  this.diceRoll = diceRoll,
   this.roundScore = roundScore,
   this.totalScore = totalScore
 }
@@ -12,17 +12,32 @@ Player.prototype.addName = function(name){
 
 Player.prototype.rollDice = function() {
   let userRoll = (Math.floor(Math.random()*6+1));
-  this.currentScore = userRoll;
-  this.roundScore += this.currentScore
+  this.diceRoll = userRoll;
+  if (userRoll === 1) {
+    this.roundScore = 0;
+  }
+  else {
+    this.roundScore += this.diceRoll
+  }
 }
 
+Player.prototype.hold = function() {
+  this.totalScore += this.roundScore;
+}
 
 // User Interface Logic
 $(document).ready(function() {
   let playerOne = new Player("Hannah", 0, 0, 0);
 
-  playerOne.rollDice();
-
-  console.log(playerOne);
-  console.log(playerOne.currentScore);
+  $("#player-one-roll").click(function() {
+    playerOne.rollDice();
+    $("#player-one-dice-roll").text(playerOne.diceRoll);
+    $("#player-one-round-score").text(playerOne.roundScore);
+  })
+  $("#player-one-hold").click(function() {
+    playerOne.hold();
+    $("#player-one-dice-roll").text(0);
+    $("#player-one-round-score").text(0);
+    $("#player-one-total-score").text(playerOne.totalScore)
+  })
 })
