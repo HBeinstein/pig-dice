57ac291609027c13a96551ee4e8c89740b23572f
Jeff Kim <kim2jy@hotmail.com>
2020-06-17 11:15:54 -0700

    Add Player object and prototype methods for adding player name and rolling dice
    Co-authored-by: Hannah Beinstein <hannah.beinstein@gmail.com>
    Co-authored-by: Ruthie Newman <ruthienewman@protonmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,31 @@
 // Business Logic
+function Player (playerName, currentScore, roundScore, totalScore){
+  this.playerName = playerName,
+  this.currentScore = currentScore,
+  this.roundScore = roundScore,
+  this.totalScore = totalScore
+}
+
+Player.prototype.addName = function(name){
+  this.playerName.push(name); 
+}
+
+Player.prototype.rollDice = function() {
+  let userRoll = (Math.floor(Math.random()*6+1));
+  this.currentScore = userRoll;
+  this.roundScore += this.currentScore
+  // this.currentScore.push(userRoll);
+}
+
+
+
 
 // User Interface Logic
 $(document).ready(function() {
+  let playerOne = new Player("Hannah", 0, 0, 0);
+  // playerOne.rollDice();
 
+  console.log(playerOne);
+  console.log(playerOne.currentScore);
   
-})e
+})
