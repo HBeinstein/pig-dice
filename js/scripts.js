bd95459db77b77a25e2742551a37dfd7e7390518
Jeff Kim <kim2jy@hotmail.com>
2020-06-17 14:05:17 -0700

    Add functionality to 'end' a player's turn by toggling the activePlayer class from player one's roll button to player two's roll button
    Co-authored-by: Hannah Beinstein <hannah.beinstein@gmail.com>
    Co-authored-by: Ruthie Newman <ruthienewman@protonmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -15,6 +15,7 @@ Player.prototype.rollDice = function() {
   this.diceRoll = userRoll;
   if (userRoll === 1) {
     this.roundScore = 0;
+    endTurnP1();
   }
   else {
     this.roundScore += this.diceRoll
@@ -25,17 +26,28 @@ Player.prototype.hold = function() {
   this.totalScore += this.roundScore;
 }
 
+const endTurn = function() {
+  document.getElementById("player-one-roll").classList.toggle("activePlayer");
+  document.getElementById("player-two-roll").classList.toggle("activePlayer");
+}
+
 // User Interface Logic
 $(document).ready(function() {
   let playerOne = new Player("Hannah", 0, 0, 0);
 
   $("#player-one-roll").click(function() {
-    playerOne.rollDice();
-    $("#player-one-dice-roll").text(playerOne.diceRoll);
-    $("#player-one-round-score").text(playerOne.roundScore);
+    if ($("#player-one-roll").hasClass("activePlayer") === true) {
+      playerOne.rollDice();
+      $("#player-one-dice-roll").text(playerOne.diceRoll);
+      $("#player-one-round-score").text(playerOne.roundScore);
+    }
+    else {
+      alert("It is not your turn!")
+    }
   })
   $("#player-one-hold").click(function() {
     playerOne.hold();
+    endTurn();
     $("#player-one-dice-roll").text(0);
     $("#player-one-round-score").text(0);
     $("#player-one-total-score").text(playerOne.totalScore)
